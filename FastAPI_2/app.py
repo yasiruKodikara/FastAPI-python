2ffbc00695424bee34dbadb0327843ee10e40909
yasiru kodikara <yasirukodikara15@gmail.com>
2026-03-23 13:23:11 +0530

    notes added

--- FastAPI_2/app.py
+++ FastAPI_2/app.py
@@ -3,9 +3,20 @@ from pydantic import BaseModel, Field, computed_field
 from typing import Literal,Annotated
 import pickle
 import pandas as pd
+from fastapi.responses import JSONResponse
+
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
 
 # import the ML model
-with open('model.pkl','rb') as f:
+with open('model.pkl','rb') as f: #open with read binary mode
     model = pickle.load(f)
 
 
@@ -15,8 +26,65 @@ app = FastAPI()
 class UserInput(BaseModel):
     age:Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
     weight:Annotated[float, Field(..., gt=0, description='Weight of the user')]
-    height:Annotated[int, Field(..., gt=0, description='Height of the user')]
-    income_lpa:Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    smoker:Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    city:Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    occupation:Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    height:Annotated[float, Field(..., gt=0, description='Height of the user')]
+    income_lpa:Annotated[float, Field(..., gt=0, lt=12000000, description='Annual salary of the user')]
+    smoker:Annotated[bool, Field(..., description='Is the user is somker?')]
+    city:Annotated[str, Field(...,description='city of the user')]
+    occupation:Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')] # type: ignore
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+        
+    @computed_field
+    @property
+    def age_group(self)-> str:
+        if self.age<25:
+            return 'young'
+        elif self.age < 45:
+                return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
+    
+    @computed_field
+    @property
+    def city_tier(self)->int:
+            if self.city in tier_1_cities:
+                return 1
+            elif self.city in tier_2_cities:
+                return 2
+            else:
+                return 3
+            
+
+@app.post('/predict')
+def predict_premium(data: UserInput):
+      
+     input_df = pd.DataFrame([
+          {
+               'bmi':data.bmi,
+               'age_group':data.age_group,
+               'lifestyle_risk':data.lifestyle_risk,
+               'city_tier':data.city_tier,
+               'income_lpa':data.income_lpa,
+               'occupation':data.occupation
+
+          }
+     ])
+
+     prediction = model.predict(input_df)[0]
+
+     return JSONResponse(status_code=200, content={'predicted_category':prediction})
